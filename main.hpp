9b07c940d74a757db7956d1efeddda2d4ab1039c
Rollin Gilman <rdgilman@my.canyons.edu>
2026-05-10 18:09:42 -0700

    a75 Find the Substring

--- main.hpp
+++ main.hpp
@@ -11,7 +11,24 @@ int finduserstring(char[], int, char[], int);
 
 int finduserstring(char cstr[], int cstrlen, char userstr[], int userlen)
 {
-    // TODO: scan cstr for the first occurrence of userstr, return its starting
+    for (int i = 0; i <= cstrlen - userlen; i++)
+    {
+        bool match = true;
+
+        for (int j = 0; j < userlen; j++)
+        {
+            if (cstr[i + j] != userstr[j])
+            {
+                match = false;
+                break;
+            }
+        }
+
+        if (match)
+            return i;
+    }
+
+    return -1;
 }
 
 #endif
